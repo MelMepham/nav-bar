38ff54e93122d8fa27cc1892ca818d3ef349eb7b
MelMepham <melissa@logmate.co.nz>
2018-08-21 15:54:45 +1200

    login page is done

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,10 @@
 import 'package:angular/angular.dart';
 import 'nav-bar/nav_bar_component.dart';
+import 'login/login_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [NavBarComponent],
+  directives: [NavBarComponent, LoginComponent],
 )
 class AppComponent {}
---
+++ lib/login/login_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'login-modal',
+  templateUrl: 'login_component.html',
+  styleUrls: ['login_component.css'],
+)
+class LoginComponent {
+}
