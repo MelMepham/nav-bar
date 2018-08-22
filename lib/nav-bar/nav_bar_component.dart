488f6043c9e95a1a634dd19ce060f56734f806b6
melmepham <m.a.mepham@gmail.com>
2018-08-22 17:30:52 +1200

    done for the day

--- lib/nav-bar/nav_bar_component.dart
+++ lib/nav-bar/nav_bar_component.dart
@@ -1,9 +1,26 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
 
 @Component(
   selector: 'nav-bar',
   templateUrl: 'nav_bar_component.html',
   styleUrls: ['nav_bar_component.css'],
+  directives: [routerDirectives, coreDirectives, formDirectives],
 )
 class NavBarComponent {
+  bool notifications = false;
+  bool menu = false;
+
+  void onClickNotifications() {
+    (menu == true) ? menu = false : null;
+    notifications = !notifications;
+    print(notifications);
+  }
+
+  void onClickMenu() {
+    (notifications == true) ? notifications = false : null;
+    menu = !menu;
+    print(menu);
+  }
 }
--- lib/register-user/register_user_component.dart
+++ lib/register-user/register_user_component.dart
@@ -1,9 +1,50 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+import '../routes.dart';
 
 @Component(
   selector: 'register-user-app',
   templateUrl: 'register_user_component.html',
   styleUrls: ['register_user_component.css'],
+  directives: [routerDirectives, coreDirectives, formDirectives],
+  exports: [RoutePaths, Routes],
 )
 class RegisterUserComponent {
+  bool passwordMatches = true;
+  bool emailValid = true;
+  bool wasSucessful = false;
+
+  NewDriver model = NewDriver(
+      'First Name',
+      'Family Name',
+      'E-mail',
+      'Password',
+      'Confirm Password');
+
+  void onSubmit() {
+    (model.password == model.confirmPassword)
+        ? passwordMatches = true
+        : passwordMatches = false;
+
+    emailValid = RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+        .hasMatch(model.email);
+
+  }
+
+  Map<String, bool> setCssValidityClass(NgControl control) {
+    final validityClass = control.valid == true ? 'is-valid' : 'is-invalid';
+    return {validityClass: true};
+  }
+}
+
+class NewDriver {
+  String firstName,
+      familyName,
+      email,
+      password,
+      confirmPassword;
+
+  NewDriver(this.firstName, this.familyName, this.email, this.password, this.confirmPassword);
 }
